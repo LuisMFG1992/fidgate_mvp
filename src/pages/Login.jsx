b16cedd9ea9b14ba1b6548de48fc749986643ab4
LuisMFG <luismiguelfloresgr@gmail.com>
2024-08-03 21:57:13 +0200

    Login page done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,16 @@
+import Logo from '../assets/Logo.svg'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
 export default function Login() {
   return (
-    <div className='w-full h-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]'>
+    <div className='w-full h-full lg:grid lg:grid-cols-2 max-w-[1920px]'>
+      {/* Form */}
       <div className='flex items-center justify-center h-full'>
-        <div className='mx-auto grid w-[350px] gap-6'>
+        <div className='grid w-[400px] gap-6'>
           <div className='grid gap-2 text-center'>
-            <h1 className='text-3xl font-bold text-primaryColor'>
+            <h1 className='text-4xl font-bold text-primaryColor'>
               Getting Started is Easy
             </h1>
             <p className='text-balance text-muted-foreground'>
@@ -41,7 +43,7 @@ export default function Login() {
               Login
             </Button>
           </div>
-          <div className='mt-4 text-center text-sm'>
+          <div className='mt-2 text-center text-sm'>
             Don{"'"}t have an account?{' '}
             <a href='#' className='underline'>
               Schedule a DEMO
@@ -49,7 +51,11 @@ export default function Login() {
           </div>
         </div>
       </div>
-      <div className='hidden lg:block bg-primaryColor'></div>
+
+      {/* Image */}
+      <div className='hidden lg:flex justify-center items-center bg-primaryColor'>
+        <img src={Logo} alt='logo' className='size-[600px]' />
+      </div>
     </div>
   )
 }
